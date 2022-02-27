7ce7182762070bb71ce98ebc11f64292cb0ac2e3
Neptune3X <52845093+Neptune3X@users.noreply.github.com>
2022-02-27 19:39:58 +0700

    save/load array set

--- save.js
+++ save.js
@@ -11,4 +11,37 @@ const data_square = []
 const data_color_square = []
 const data_rectangle = []
 const data_color_rectangle = []
-const data
+const data_polygon = []
+const data_color_polygon = []
+
+const saveData1 = (data_line) =>{
+    const jsonData1 = JSON.stringify(data_line)
+}
+
+const saveData2 = (data_color_line) =>{
+    const jsonData2 = JSON.stringify(data_color_line)
+}
+
+const saveData3 = (data_square) =>{
+    const jsonData3 = JSON.stringify(data_square)
+}
+
+const saveData4 = (data_color_square) =>{
+    const jsonData4 = JSON.stringify(data_color_square)
+}
+
+const saveData5 = (data_rectangle) =>{
+    const jsonData5 = JSON.stringify(data_rectangle)
+}
+
+const saveData6 = (data_color_rectangle) =>{
+    const jsonData6 = JSON.stringify(data_color_rectangle)
+}
+
+const saveData7 = (data_polygon) =>{
+    const jsonData7 = JSON.stringify(data_polygon)
+}
+
+const saveData8 = (data_color_polygon) =>{
+    const jsonData8 = JSON.stringify(data_color_polygon)
+}
